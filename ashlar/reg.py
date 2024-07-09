Correct radian conversion in final tform_rotation definition

ashlar/reg.py
@@ -1013,7 +1013,9 @@ class LayerAligner(object):
         if self.verbose:
             print(f"    refined cycle rotation = {angle:.2f} degrees")
         self.cycle_angle_fine = angle
-        self.tform_rotation = skimage.transform.AffineTransform(rotation=angle)
+        self.tform_rotation = skimage.transform.AffineTransform(
+            rotation=np.deg2rad(angle),
+        )
 
     def intersection(self, t):
         center_a = self.reference_centers[t]